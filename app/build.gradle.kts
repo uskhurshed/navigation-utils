b7ff5d2617444cbde7323965b4e0713068cc71c0
khurshed <khurshed.apps@gmail.com>
2025-03-28 15:14:57 +0500

    -

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,13 +14,6 @@ android {
         versionCode = 1
         versionName = "1.0"
     }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-        }
-    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
