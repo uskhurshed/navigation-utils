bd5319060a18539936102696605de1307284f4b7
khurshed <khurshed.apps@gmail.com>
2025-03-28 15:06:51 +0500

    -

--- navigation/build.gradle.kts
+++ navigation/build.gradle.kts
@@ -9,14 +9,6 @@ android {
 
     defaultConfig {
         minSdk = 24
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-        }
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
